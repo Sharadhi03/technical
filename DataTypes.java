52c6b7a1422fd7ef91052cc1c9b77cc9650080be
Sharadhi Shedigumme <118157653+Sharadhi03@users.noreply.github.com>
2024-01-11 16:25:24 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Declare variables of different data types and assign values to them.
+        byte myByte = 127;
+        short myShort = 32767;
+        int myInt=123;
+        long myLong = 98765432;
+        float myFloat =0.1904f;
+        double myDouble = 0.1904;
+        char mychar='1';
+        System.out.println("The value of myByte is " +myByte);
+        System.out.println("The value of myShort is "+myShort);
+        System.out.println("The value of myInt is "+myInt);
+        System.out.println("The value of myLong is "+myLong);
+        System.out.println("The value of myFloat is"+myFloat);
+        System.out.println("The value of myDouble is "+myDouble);
+        System.out.println("The value of mychar is "+mychar);
+
+    }
+    
+}
